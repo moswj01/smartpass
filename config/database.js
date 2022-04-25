bc9beb5a66eaf1928b01df8ea72a3d0f13e1eea6
moswj_10 <watchara@centralsiam.com>
2022-04-25 14:46:51 +0700

    Update Database Config

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', 'bungki_wallet_uat'),
+      user: env('DATABASE_USERNAME', 'bungkie-dev'),
+      password: env('DATABASE_PASSWORD', 'sYHeeGrHddST'),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
